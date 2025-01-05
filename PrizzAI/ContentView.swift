e58147268b3885332404ff509de3a0ad66e3329b
cjb325 <98486817+cjb325@users.noreply.github.com>
2025-01-04 20:24:36 -0500

    Initial Commit

---
+++ PrizzAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrizzAI
+//
+//  Created by Chris Benincasa on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrizzAI/PrizzAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrizzAIApp.swift
+//  PrizzAI
+//
+//  Created by Chris Benincasa on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PrizzAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
